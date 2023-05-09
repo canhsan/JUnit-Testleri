dc082c27ea9d02875af9fdb2bda93bd80cb4dd12
canhsan <1979hasancan@gmail.com>
2023-05-09 18:59:15 +0300

    5.09/05/2023

---
+++ src/test/java/day06_JUnitFramework/C04_AssertionMethodlari.java
@@ -0,0 +1,42 @@
+package day06_JUnitFramework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_AssertionMethodlari {
+    /*
+        1) Bir class oluşturun: BestBuyAssertions
+        2) https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak
+        asagidaki testleri yapin
+        ○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
+        ○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+        ○ logoTest => BestBuy logosunun görüntülendigini test edin
+        ○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
+     */
+    WebDriver driver;
+
+    public void mahserin4Atlisi(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.bestbuy.com");
+
+    }
+    @Test
+    public void test01() throws InterruptedException {
+    // Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
+        mahserin4Atlisi();
+        String expectedUrl="https://www.bestbuy.com/";
+        String actualUrl=driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl,actualUrl);
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
